9f212476fed0983d1de384e64263e583444490ba
Nicole Vingerhoeds <ving0010@algonquinlive.com>
2024-10-25 13:59:47 -0400

    add dms2dd function and tests

--- NicoleV/dms_converter.py
+++ NicoleV/dms_converter.py
@@ -1,37 +1,57 @@
 def dms2dd(record):
-    record_list = record.split()
     record_strip = record.strip()
-
-    possible_long_directions = ["W", "w", "E", "e"]
-
-    find_long_sign = [record_strip[i].isalpha() for i in record_strip]
-    #long_substr = record_strip[:record_strip.find(possible_long_directions)]
-
-    # find where the w or e character is in the string
-    for i in len(record_strip):
+    # find the first alphabetic character in the string
+    for i in range(len(record_strip)):
         if record_strip[i].isalpha():
             break
-        else:
-            continue
-    return i
+    long_dir = record_strip[i].upper()
+    long_substr = record_strip[:i].strip()
 
-    long_sign = record_strip[i]
+    try: 
+        long_deg = int(long_substr.split()[0])
+    except IndexError:
+        long_deg = 0
+    try: 
+        long_min = int(long_substr.split()[1])
+    except IndexError:
+        long_min = 0
+    try: 
+        long_sec = int(long_substr.split()[2])
+    except IndexError:
+        long_sec = 0   
 
-    # long_deg
-    # long_min
-    # long_sec
-    # long_sign
+    # find the second alphabetic character in the string
+    for j in range(i+1, len(record_strip)):
+        if record_strip[j].isalpha():
+            break
+    lat_dir = record_strip[j].upper()
+    lat_substr = record_strip[i+1:j].strip()
+    
+    try:
+        lat_deg = int(lat_substr.split()[0])
+    except IndexError:
+        lat_deg =0
+    try:
+        lat_min = int(lat_substr.split()[1])
+    except IndexError:
+        lat_min = 0
+    try:
+        lat_sec = int(lat_substr.split()[2])
+    except IndexError:
+        lat_sec = 0
 
-    # lat_deg
-    # lat_min
-    # lat_sec
-    # lat_sign
+    if all([0<=long_deg<=180, 0<=long_min<=59,  0<=long_sec<=59,
+            long_dir in ('W', 'E'), lat_dir in ('N', 'S'),
+            0<= lat_deg <= 90, 0<=lat_min<=59, 0<=lat_sec<=59]):
+        #then calculate long degree
+        long_dd = long_deg + (long_min/60) + (long_sec/3600)
+        lat_dd = lat_deg + (lat_min/60) + (lat_sec/3600)
+        if long_dir == 'W': long_dd *= -1
+        if lat_dir == 'S': lat_dd *= -1
+    else:
+        long_dd, lat_dd = None, None
 
-    long, lat = "", ""
-
-
-
-    return long, lat
+    return long_dd, lat_dd
 
 # running pytest in the terminal is helpful if there are 
 # problems getting the TDD framework set up
--- NicoleV/test_dms_converter.py
+++ NicoleV/test_dms_converter.py
@@ -1,8 +1,73 @@
 import dms_converter as d
 
-def test_dms2dd():
+def test_dms2dd_valid_input_WN():
     test_input = "075 45 03 W 45 23 05 N\n"
-    expected = -75.75083333, 45.38472222
+    expected = -(75+(45/60)+(3/3600)), 45+(23/60)+(5/3600)
     actual = d.dms2dd(test_input)
     assert expected == actual
 
+def test_dms2dd_valid_input_WS():
+    test_input = "075 45 03 W 45 23 05 S\n"
+    expected = -(75+(45/60)+(3/3600)), -(45+(23/60)+(5/3600))
+    actual = d.dms2dd(test_input)
+    assert expected == actual
+
+def test_dms2dd_valid_input_ES():
+    test_input = "075 45 03 E 45 23 05 S\n"
+    expected = (75+(45/60)+(3/3600)), -(45+(23/60)+(5/3600))
+    actual = d.dms2dd(test_input)
+    assert expected == actual
+
+def test_dms2dd_valid_input_EN():
+    test_input = "075 45 03 E 45 23 05 N\n"
+    expected = (75+(45/60)+(3/3600)), (45+(23/60)+(5/3600))
+    actual = d.dms2dd(test_input)
+    assert expected == actual
+
+def test_dms2dd_valid_input_lowercase():
+    test_input = "075 45 03 w 45 23 05 n\n"
+    expected = -(75+(45/60)+(3/3600)), 45+(23/60)+(5/3600)
+    actual = d.dms2dd(test_input)
+    assert expected == actual
+
+def test_dms2dd_long_deg_out_of_range():
+    test_input = "181 45 03 W 45 23 05 N\n"
+    expected = None,None
+    actual = d.dms2dd(test_input)
+    assert expected == actual
+
+def test_dms2dd_long_min_out_of_range():
+    test_input = "075 60 03 W 45 23 05 N\n"
+    expected = None,None
+    actual = d.dms2dd(test_input)
+    assert expected == actual
+
+def test_dms2dd_weird_letter():
+    test_input = "075 45 03 j 45 23 05 N\n"
+    expected = None,None
+    actual = d.dms2dd(test_input)
+    assert expected == actual    
+
+def test_dms2dd_no_long_sec():
+    test_input = "075 45 W 45 23 05 N\n"
+    expected = -(75+(45/60)), 45+(23/60)+(5/3600)
+    actual = d.dms2dd(test_input)
+    assert expected == actual   
+
+def test_dms2dd_no_long_sec_min():
+    test_input = "075 W 45 23 05 N\n"
+    expected = -(75), 45+(23/60)+(5/3600)
+    actual = d.dms2dd(test_input)
+    assert expected == actual  
+
+def test_dms2dd_no_lat_sec():
+    test_input = "075 45 03 W 45 23 N\n"
+    expected = -(75+(45/60)+(3/3600)), 45+(23/60)
+    actual = d.dms2dd(test_input)
+    assert expected == actual   
+
+def test_dms2dd_no_lat_sec_min():
+    test_input = "075 45 03 W 45 N\n"
+    expected = -(75+(45/60)+(3/3600)), 45
+    actual = d.dms2dd(test_input)
+    assert expected == actual  
